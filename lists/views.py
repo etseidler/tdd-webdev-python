80b02d22a474a6f14d5a709784b579a1745b38f3
Eric Seidler <eric.seidler@gmail.com>
2014-03-06 12:20:19 -0600

    rename all item input ids and names. still broken

--- lists/views.py
+++ lists/views.py
@@ -13,7 +13,7 @@ def view_list(request, list_id):
 
     if request.method == 'POST':
         try:
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
@@ -23,7 +23,7 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
         item.save()
@@ -33,9 +33,3 @@ def new_list(request):
         return render(request, 'home.html', {"error": error})
     return redirect(list_)
 
-'''
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
-'''
